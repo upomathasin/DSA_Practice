3dbeb8c2149374b1205852ffb02bacae7527c4f3
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-03-05 00:16:50 +0600

    first commit

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,7 +1,17 @@
 const array = [10, 30, 4, 3, 1, 40, 0];
 
-console.log(array);
-
+{
+  /**
+     * 
+     * Bubble sort: 
+Go through the array, one value at a time.
+For each value, compare the value with the next value.
+If the value is higher than the next one, swap the values so that the highest value comes last.
+Go through the array as many times as there are values in the array.
+     * 
+     * 
+     */
+}
 for (var j = 0; j < array.length; j++) {
   for (var i = 0; i < array.length - j; i++) {
     if (array[i] > array[i + 1]) {
