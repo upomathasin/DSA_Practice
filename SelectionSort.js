800de7ca5b449f5650eb0a7721a1c2c7ef8355c9
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-03-06 13:14:44 +0600

    selection sort

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,3 +1,11 @@
+{
+  /**    
+Go through the array to find the lowest value.
+Move the lowest value to the front of the unsorted part of the array.
+Go through the array again as many times as there are values in the array.   
+**/
+}
+
 const a = [10, 30, 4, 3, 1, 40, 0];
 
 for (var i = 0; i < a.length; i++) {
